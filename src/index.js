be4a111b26dbed0f182da4a33a07a13133e9f1ac
uniq47 <unique.sapkota01@student.csulb.edu>
2023-02-11 06:55:59 -0800

    refactor or cleaned the yarn installatin

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import {StrictMode} from 'react';
+import ReactDOM from 'react-dom/client';
+
+import App from './App';
+
+ReactDOM.createRoot(document.getElementById('root')).render(<StrictMode><div>Hello</div></StrictMode>);
+
+// const destination = document.getElementById('root');
+// const sourceCode = <div>Hello</div>
+
+// const getDest = ReactDOM.createRoot(destination);
+// getDest.render(sourceCode);
+
+
+
