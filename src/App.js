88335f6e8243996aa6c3066aeebfd6d0838d26ee
uniq47 <unique.sapkota01@student.csulb.edu>
2023-02-12 09:45:07 -0800

    added css

--- src/App.js
+++ src/App.js
@@ -4,26 +4,37 @@ import illustration_woman_mobile from "./images/illustration-woman-online-mobile
 
 const App = () => {
   return (
-    <div>
-      <div>
-        <img src={illustration_box} alt="illustration with box" />
-        <image src={illustration_woman_desktop} alt="illustration with women" />
-        <image src={illustration_woman_mobile} alt="illustration with women" />
-      </div>
-      <div>
-        <h1> FAQ</h1>
-        <dl>
-          <div>
-            <dt>
-                <button> My First Question</button>
-            </dt>
-            <dd>
-                <p>My first Answer</p>
-            </dd>
+    <main className="container">
+      <article className="component">
+        
+          <div className="illustration">
+            <img src={illustration_box} alt="illustration with box"  className="illustration__box"/>
+            <img
+              src={illustration_woman_desktop}
+              alt="illustration with women" className="illustration__women_dekstop"
+            />
+            <img
+              src={illustration_woman_mobile}
+              alt="illustration with women"  className="illustration__women_mobile"
+            />
           </div>
-        </dl>
-      </div>
-    </div>
+
+          <div>
+            <h1> FAQ</h1>
+            <dl>
+              <div>
+                <dt>
+                  <button> My First Question</button>
+                </dt>
+                <dd>
+                  <p>My first Answer</p>
+                </dd>
+              </div>
+            </dl>
+          </div>
+        
+      </article>
+    </main>
   );
 };
 
