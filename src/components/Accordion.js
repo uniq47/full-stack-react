f2ab6e222d4a09ad676c84fb96c35eb7df082398
uniq47 <unique.sapkota01@student.csulb.edu>
2023-02-13 19:33:05 -0800

    got the props from one component to another

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -17,21 +17,19 @@ function Accordion({ questionAnswers }) {
       {/* writing Accordion here is like writting <div>Accordion</div> */}
 
       <dl>
-        {questionAnswers.map((item) => {
-          // <Accordionitem />>
-          return <div>
-            <div>
-            {item.question}
-            </div>
-            <div>
-            {item.answer}
-            </div>
-          </div>
+        {questionAnswers.map((item,index) => {
+         
+          return  <Accordionitem qa ={item} key = {index} />
         })}
         
       </dl>
     </div>
   );
-}
-
+}// passed a prop form accordion to accordionitem <Accordionitem qa ={item} key = {index} />
+// when mapping a component with out passing a key prop react will give a warning saying that we need to pass a key prop
+// when mapping in react we need to write a key prop here aso react will understand whioch one isthe firs second andquestoin
+// with out passing hte ke prop it meyu not usderstand clearly 
+// key should be unique so we pass unque elemnt each and everytime here  Accordionitem key = {here}
+// index starts at 0 and goes at the end of the array its always unoque so we cna write index here  Accordionitem key = {index}
+// most of the tome we getit from api or some time we generate , we need to pass the key or we will see the error.Each child in a list should have a unique "key" prop. 
 export default Accordion;
--- src/components/Accordionitem.js
+++ src/components/Accordionitem.js
@@ -1,18 +1,29 @@
-function Accordionitem() {
 
-
+// destructuring the props we can do this {question } but we cant 
+// as WE NEED TO , WE CAN WRITE ANY NAME WE CAN WRITE ABCDEF INSTED OF QA
+//WE WRITE IT PROPS, PROPS CONTAINS THE QUESTION AND ANSWER 
+// so first we destructure the qa where it will contain the question ans answers
+function Accordionitem({qa: {question,answer}}) {
+  //const{question,answer} = qa;
+// so  we destructured the qa and we got the question and answer
+// we can do nexted destructuring like this const{question,answer} = qa;
+//  className ="faq__desc" we apply css with inthe click of the question button the answer should be shown
   return (
     <div className="faq__question">
       <dt>
-        <button className ="faq__question-button"> My First Question</button>
+        <button className ="faq__question-button"> {question}</button>
       </dt>
-      <dd className ="faq__desc">
-        <p>My first Answer</p> 
+      <dd >
+        <p className ="faq__descj">{answer}</p> 
       </dd>
     </div>
   );
 }
-
+// how to revieve the prop, in line number one we need to distructure the prop which is qa
+// we wont get hte questoin and answer here because we are not passing the props here
+// we need to pass the props here so , pass it from the pasrents
+// accordtion is the parent component of accordionitem so we need to pass the props from accordion to accordionitem
+//
 export default Accordionitem;
 
 // Accordionitem.js goes inside Accordion.js and Accordion.js goes inside App.js
